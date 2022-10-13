cbf3d591275058ff615bae24d1d2058687352d63
Katie McTaggart <katiemctaggartengineer@gmail.com>
2022-10-12 21:36:08 -0400

    Create win conditions in checkForWin()

--- main.cpp
+++ main.cpp
@@ -27,7 +27,74 @@ void printBoard(){
                 cout << board[j][i] << " | " ;
             }
         }
+
+        if(j < rows-1){
+            cout << "----------" << endl;
+        }
     }
+}
+
+void checkForWin(){
+    int j;
+    int i;
+    bool win = false;
+    
+    // Obtain the size of the tik tac toe board
+    int rows = sizeof board / sizeof board[0];
+    int cols = sizeof board[0] / sizeof(char);
+
+    // Checks all rows for wins
+    for(j = 0; j < rows; j++){
+        // Checks if all values in a row are equal to each other 
+        for(i = 0; i < cols-1; i++){        
+            if(board[j][i] != board[j][i+1]){
+                win = false;
+                break;
+            }
+            else{
+                win = true;
+            }
+        }
+    }
+
+    // Checks all cols for wins
+    for(i = 0; i < cols; i++){
+        // Checks if all values in a row are equal to each other 
+        for(j = 0; j < rows-1; j++){        
+            if(board[j][i] != board[j][i+1]){
+                win = false;
+                break;
+            }
+            else{
+                win = true;
+            }
+        }
+    }
+        
+    // Check main diagonal
+    for(j=0; j < rows-1; j++){
+        if(board[j][j] != board[j+1][j+1]){
+            win = false;
+            break;
+        }
+        else{
+            win = true;
+        }
+
+    }
+
+    // Check other diagonal
+    for(j=0; j < rows-1; j++){
+        if(board[j][rows-1-j] != board[j+1][rows-j]){
+            win = false;
+            break;
+        }
+        else{
+            win = true;
+        }
+
+    }
+
 }
 
 
@@ -36,6 +103,7 @@ int main(){
     int boxSelection;
     char player1;
     char player2;
+    int move;
 
     cout << "Would you like to be \'X\' or \'O\')? ";
     cin >> player1;
@@ -49,6 +117,12 @@ int main(){
     else{
         cout << "Please input only \'X' or \'O'" << endl;
     }
+
+
+    cout << "Make your move ";
+    cin >> move;
+
+    checkForWin();
 
 
     //createBoard();
